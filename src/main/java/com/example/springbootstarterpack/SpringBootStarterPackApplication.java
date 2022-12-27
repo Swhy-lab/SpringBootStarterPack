586f5f4e7bef42c722271694975a78f50938d212
seung <Seung.workspace@gmail.com>
2022-12-27 15:40:12 +0900

    v202212271540

---
+++ src/main/java/com/example/springbootstarterpack/SpringBootStarterPackApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootstarterpack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStarterPackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStarterPackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootstarterpack/SpringBootStarterPackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootstarterpack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStarterPackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
